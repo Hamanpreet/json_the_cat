fbaca0b03a19c0466408b00874dbe1c3be5a6751
Hamanpreet <haman42626@gmail.com>
2023-05-19 17:04:50 -0400

    Handle wrong URL and wrong breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,20 @@
 const request = require('request');
 const breed = process.argv[2];
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,(error,response,body) => {
-  if (error) {
-    return console.log(error);
+  if (error !== null) {
+    console.log(error , [error])
+    return console.log("URL not found");
   }
   const data = JSON.parse(body);
   
-  if (data.length === 0) {
-    console.log("Breed not found");
+  if (data && data.length !== 0) {
+    console.log(data[0].description);
   }
-  console.log(data[0].description);
-  
+ 
+  return console.log("Breed not found");
 });
+
+
+
+//exporting the function
+//module.exports = fetchBreedDescription
